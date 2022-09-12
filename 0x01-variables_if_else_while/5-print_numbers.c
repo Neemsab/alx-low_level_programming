c3194255e9b6a03b6a2e029bb657b7d4bb499ce1
Neemsab <elneems74@gmail.com>
2022-09-11 17:01:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print lowercase alpha a-z but remove 'q' and 'e'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
