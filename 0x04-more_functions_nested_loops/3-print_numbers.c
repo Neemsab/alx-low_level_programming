dfccfeb4950485bae70f2111301b08f4faee7ea2
Neemsab <elneems74@gmail.com>
2022-09-16 09:44:14 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - check the code
+* Return: Always 0 
+*/
+#include "main.h"
+
+void print_numbers(void)
+{
+	char i;
+	
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	
+	_putchar('\n');
+}
