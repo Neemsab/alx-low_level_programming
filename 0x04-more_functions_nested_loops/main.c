8617784d37e116a5d08cd5659a3ee63f8d61fe02
Neemsab <elneems74@gmail.com>
2022-09-15 21:31:12 -0700

    main

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototype
+ * int prototypes (int
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print _square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(chart);
+int _isupper(int);
+int _isdigit(int);
+int Mul(int, int);
+
+#endif /* MAIN_H */
